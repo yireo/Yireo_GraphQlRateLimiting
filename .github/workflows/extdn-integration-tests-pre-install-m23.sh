f2f45b0faa9071e4e5ec381b601f4d0e7011ea47
jisse Reitsma <jisse@yireo.com>
2022-02-09 16:45:26 +0100

    Require integration helper

--- .github/workflows/extdn-integration-tests-pre-install-m23.sh
+++ .github/workflows/extdn-integration-tests-pre-install-m23.sh
@@ -2,5 +2,6 @@
 composer config minimum-stability dev
 composer config prefer-stable false
 
+composer require yireo/magento2-integration-test-helper:@dev
 composer require yireo/magento2-replace-bundled:^3.0 --no-update
 #composer require yireo/magento2-replace-test:@dev --no-update
